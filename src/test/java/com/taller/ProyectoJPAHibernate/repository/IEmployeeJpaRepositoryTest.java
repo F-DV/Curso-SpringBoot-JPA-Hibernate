d5e67ab4151e0b23f8d5777e1af97bed619bc60f
Felipe <fquiceno1@misena.edu.co>
2022-02-20 22:31:22 -0500

    Creamos dos perfiles application.properties para conexiones a base de datos, mysql y H2 Se agrega dependencia H2 en pom.xml

--- src/test/java/com/taller/ProyectoJPAHibernate/repository/IEmployeeJpaRepositoryTest.java
+++ src/test/java/com/taller/ProyectoJPAHibernate/repository/IEmployeeJpaRepositoryTest.java
@@ -3,19 +3,23 @@ package com.taller.ProyectoJPAHibernate.repository;
 import com.taller.ProyectoJPAHibernate.model.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@AutoConfigureTestDatabase (replace = AutoConfigureTestDatabase.Replace.NONE)
 class IEmployeeJpaRepositoryTest {
 /**
  * @DataJpaTest :  le indico que solo realizer test para Jpa
- * AutoConfigureTestDatabase :
+ * AutoConfigureTestDatabase (replace = AutoConfigureTestDatabase.Replace.NONE) : Utiliza la base de datos mysql para hacer los test
+ * @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)  : utiliza la base de datos embebida para los test
  * private IEmployeeJpaRepository repo;  :Inyectamos implementacion de repository
- *  repo.flush(); : inserta los empleados de inmediato
+ * repo.flush(); : inserta los empleados de inmediato
+ *
  */
     @Autowired
     private IEmployeeJpaRepository repo;
