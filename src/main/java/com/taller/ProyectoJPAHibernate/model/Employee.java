a3c2d8952a6d4aafef3fa4a3fd4a0ec2f70dd8f1
Felipe <fquiceno1@misena.edu.co>
2022-02-21 16:33:30 -0500

    Instanciamos Projects y Role como ForeingKey

--- src/main/java/com/taller/ProyectoJPAHibernate/model/Employee.java
+++ src/main/java/com/taller/ProyectoJPAHibernate/model/Employee.java
@@ -1,6 +1,8 @@
 package com.taller.ProyectoJPAHibernate.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -10,6 +12,8 @@ public class Employee {
      * @GeneratedValue(strategy = GenerationType.AUTO) : Autoincremente cada que se crea un id
      * nullable = false : indica campo no null
      * unique = true : Indica campo unico
+     * @ManyToOne(optional = false) : Indica que esta tabla instanciad a ssera uno a muchos
+     *@JoinColumn(name = "id_role" : especifica la foreing key de la tabla Role
      */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -24,13 +28,29 @@ public class Employee {
     @Column(length = 10,nullable = false, unique = true)
     private String employeeId;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "id_role")
+    private Role role;
+
+    /**
+     * Tabla intermedia de muchos a muchos, la cual tendra las dos foreing key de
+     * employee y project
+     */
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "employee_project",
+            joinColumns = { @JoinColumn(name = "employee_id") },
+            inverseJoinColumns = { @JoinColumn(name = "project_id")})
+    private List<Project> projects = new ArrayList<Project>();
+
+
     public Employee() {
     }
 
-    public Employee(String firstName, String lastName, String employeeId) {
+    public Employee(String firstName, String lastName, String employeeId,Role role) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.employeeId = employeeId;
+        this.role = role;
     }
 
     public Long getId() {
@@ -64,6 +84,23 @@ public class Employee {
     public void setEmployeeId(String employeeId) {
         this.employeeId = employeeId;
     }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
 
     @Override
     public boolean equals(Object o) {
