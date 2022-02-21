124f0c218a8635e5016fb864e8fb42ac4d82decc
Felipe <fquiceno1@misena.edu.co>
2022-02-21 16:29:08 -0500

    Declaramos metodos para buscar por apellido y buscar por role

--- src/main/java/com/taller/ProyectoJPAHibernate/repository/IEmployeeJpaRepository.java
+++ src/main/java/com/taller/ProyectoJPAHibernate/repository/IEmployeeJpaRepository.java
@@ -1,8 +1,11 @@
 package com.taller.ProyectoJPAHibernate.repository;
 
 import com.taller.ProyectoJPAHibernate.model.Employee;
+import com.taller.ProyectoJPAHibernate.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
@@ -14,4 +17,18 @@ public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
      * @return : Type Employee
      */
     Employee findByEmployeeId(String employee);
+
+    /**
+     * Pretende buscar por apellido ingresado
+     * @param lastName : String de apellifo
+     * @return Lista con apellidos
+     */
+    List<Employee> findbyeLastName(String lastName);
+
+    /**
+     *
+     * @param role : recibe objeto de tipo Role a buscar
+     * @return : Retorna lista que concuerda con ese rol ingresado
+     */
+    List<Employee> findByRole(Role role);
 }
