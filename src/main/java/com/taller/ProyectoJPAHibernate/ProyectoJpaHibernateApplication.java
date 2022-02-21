1c3f93299fad02eebdd058a93ca75c0f1ad533a3
Felipe <fquiceno1@misena.edu.co>
2022-02-20 20:55:27 -0500

    Sincroniza con repositorio remoto

---
+++ src/main/java/com/taller/ProyectoJPAHibernate/ProyectoJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.taller.ProyectoJPAHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoJpaHibernateApplication.class, args);
+	}
+
+}
