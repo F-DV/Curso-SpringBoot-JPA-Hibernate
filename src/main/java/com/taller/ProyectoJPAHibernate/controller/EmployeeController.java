81862167a5afe7be1542098ef3069a17fd80648c
Felipe <fquiceno1@misena.edu.co>
2022-02-22 17:36:51 -0500

    se creo CRUD en Employee,metodos para eliminar por id, crear, actualizar,eliminar

--- src/main/java/com/taller/ProyectoJPAHibernate/controller/EmployeeController.java
+++ src/main/java/com/taller/ProyectoJPAHibernate/controller/EmployeeController.java
@@ -3,8 +3,12 @@ package com.taller.ProyectoJPAHibernate.controller;
 import com.taller.ProyectoJPAHibernate.model.Employee;
 import com.taller.ProyectoJPAHibernate.model.Role;
 import com.taller.ProyectoJPAHibernate.service.EmployeeService;
+import com.taller.ProyectoJPAHibernate.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/empleados")
@@ -14,15 +18,52 @@ public class EmployeeController {
     EmployeeService employeeService;
 
     /**
-     * Guarda empleado en base de datos
-     * @param empleado : recibe empleado a guardar
-     * @return : retorna empleado guardado
+     * Crea y actualiza empleado en base de datos , especificando el id
+     * @param empleado: recibe datos de empleado crear o actualizar
+     * @return : retorna empleado creado o actualizado
      */
-    @PostMapping(path = "/{role}")
-    public Employee guardarEmpleado(@RequestBody Employee empleado,@PathVariable("role") Role role){
-
+    @PostMapping()
+    public Employee actualizaEmpleado(@RequestBody Employee empleado){
         return this.employeeService.guardarEmpleado(empleado);
     }
+
+    /**
+     * Obtener a todos los empleados de de la base de datos
+     * @return : ArrayList de todos los empleados
+     */
+    @GetMapping("/traeEmpleados")
+    public ArrayList<Employee> obtenerTodos(){
+        return employeeService.traeTodos();
+    }
+
+    /**
+     * Busca usuario ingresandole el id, en el servicio
+     * @param id : id del usuario a buscar
+     * @return: el empleado que corresponde a ese id ingresado
+     */
+    @GetMapping(path = "/{id}")
+    public Optional<Employee> obtenerPorId(@PathVariable("id") Long id){
+        return this.employeeService.obtenerEmpleadoPorId(id);
+    }
+
+    /**
+     * Metodo para eliminar empleado por id
+     * @param id : el id del empleado a eliminar
+     * @return: retorna mensae de confirmacion o negacion de eliminacion
+     */
+    @DeleteMapping(path = "/{id}")
+    public String eliminarEmpleadopPorId(@PathVariable("id") Long id){
+       boolean eliminado = this.employeeService.eliminarEmpleado(id);
+
+       if(eliminado){
+           return "Empleado eliminado correctamente" ;
+       }else
+
+            return "El empleado no se elimino";
+    }
+
+
+
 
 
 }
--- src/main/java/com/taller/ProyectoJPAHibernate/service/EmployeeService.java
+++ src/main/java/com/taller/ProyectoJPAHibernate/service/EmployeeService.java
@@ -1,10 +1,14 @@
 package com.taller.ProyectoJPAHibernate.service;
 
 import com.taller.ProyectoJPAHibernate.model.Employee;
+import com.taller.ProyectoJPAHibernate.model.Role;
 import com.taller.ProyectoJPAHibernate.repository.IEmployeeJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
@@ -20,15 +24,48 @@ public class EmployeeService {
     }
 
     /**
-     * Guarda empleado en base de datos
-     * @param employee :  recibe empleado a guardar con todos sus datos
-     * @return : retorna empleado guardado
+     * Crea empleado en base de datos
+     * @param employee :  recibe empleado a gcrear con todos sus datos
+     * @return : retorna empleado creado
      */
     public Employee guardarEmpleado(Employee employee){
 
         iEmployeeJpaRepository.save(employee);
         return employee;
     }
+
+    /**
+     * Obtiene a todos los empleados de de la base de datos
+     * @return : ArrayList de todos los empleados
+     */
+    public ArrayList<Employee> traeTodos(){
+        return (ArrayList<Employee>) iEmployeeJpaRepository.findAll();
+    }
+
+    /**
+     * Busca usuario ingresandole el id en el repositorio
+     * @param id : id del usuario a buscar
+     * @return: el empleado que corresponde a ese id ingresado
+     */
+    public Optional<Employee> obtenerEmpleadoPorId(long id){
+        return iEmployeeJpaRepository.findById(id);
+    }
+
+    /**
+     * Eliminacion de usuario en el repositorio por id
+     * @param id  : del empleado a eliminar en el repositorio
+     * @return . retorna verdadero o falso si fue eliminado o no el empleado
+     */
+    public boolean eliminarEmpleado(long id){
+        try{
+            iEmployeeJpaRepository.deleteById(id);
+            return true;
+        }catch(Exception err){
+            return false;
+        }
+    }
+
+
 
 
 }
